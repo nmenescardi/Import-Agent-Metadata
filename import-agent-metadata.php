752f053891b4bff482ebc9324a4104a1f78e3a56
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-03-22 07:47:05 -0400

    Change default values

--- import-agent-metadata.php
+++ import-agent-metadata.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:     Import Agent Metadata
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      http://github.com/nmenescardi/Import-Agent-Metadata
+ * Description:     This plugin is used to import specific metadata related with Agents CPT.
+ * Author:          Nicolas Menescardi
+ * Author URI:      http://github.com/nmenescardi
  * Text Domain:     import-agent-metadata
  * Domain Path:     /languages
  * Version:         0.1.0
@@ -12,4 +12,4 @@
  * @package         Import_Agent_Metadata
  */
 
-// Your code starts here.
+ 
