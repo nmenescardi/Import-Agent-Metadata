04bab42ec524dc73ff3ef2743b1536e268e0a0a1
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-03-22 07:37:30 -0400

    initial commit - wp scaffold structure

---
+++ import-agent-metadata.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Import Agent Metadata
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     import-agent-metadata
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Import_Agent_Metadata
+ */
+
+// Your code starts here.
