7062ad06d10228a554c961121b9b42acef85391f
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-03-22 16:54:49 -0400

    Update DB - Transient for DB updated - Refactoring

--- src/IAM.php
+++ src/IAM.php
@@ -8,7 +8,11 @@ use \IAM\CSVFile;
 class IAM {
 
   const TRANSIENT_CSV_ROWS = 'csv_rows_processed';
-  const TRANSIENT_EXPIRATION = 'MINUTE_IN_SECONDS';
+  const TRANSIENT_EXPIRATION_CSV = 'HOUR_IN_SECONDS';
+
+  const TRANSIENT_DB_UPDATED = 'agent_id_updated';
+  const TRANSIENT_EXPIRATION_DB_UPDATED = 'HOUR_IN_SECONDS';
+  const TRANSIENT_DB_UPDATED_LABEL = 'AGENT_ID_UPDATED';
 
   private $plugin_path;
 
@@ -26,32 +30,93 @@ class IAM {
   }
 
   public function process(){
-    $this->processCSVFile();
+    $rows = $this->processCSVFile();
 
-    //$this->updateDB();
+    $this->updateDB( $rows );
   }
   
   private function processCSVFile(){
 
-    $rows = $this->getTransient();
+    $rows = $this->getTransientCSV();
     if( empty( $rows ) ) {
       $rows = $this->csv_file->getRows();
-      $this->setTransient($rows);
+      $this->setTransientCSV($rows);
 
       $this->logger->logInfo('Transient Expired. Reading CSV successfully');
     } else {
       
       $this->logger->logInfo('Returning rows in Transient');
     }
-    //print_r( $rows );
+
+    return $rows;
   }
 
-  private function getTransient(){
+  
+  private function get_post_id_by_meta_key_and_value($key, $value) {
+	  global $wpdb;
+  
+    $meta = $wpdb->get_results("SELECT * FROM `".$wpdb->postmeta."` WHERE meta_key='".$wpdb->escape($key)."' AND meta_value='".$wpdb->escape($value)."'");
+  
+    if (is_array($meta) && !empty($meta) && isset($meta[0])) {
+	  	$meta = $meta[0];
+		}	
+    
+    if ( is_object($meta) ) {
+      
+      $this->logger->logInfo( 'Agent Found for email: ' . $value );
+      return $meta->post_id;
+      
+		}	else {
+      
+      $this->logger->logWarning( 'Agent NOT Found for email: ' . $value );
+		  return false;
+		}
+	}
+
+
+  private function updateDB( $rows ){
+    $header = array_shift( $rows);
+    
+    foreach( $rows as $agent ){
+
+      $email = $agent[2];
+      $name = $agent[1];
+      $userId = $agent[0];
+      $id = $this->get_post_id_by_meta_key_and_value( 'our_ao-aema', $email );
+    
+      if ( false !== $id && '' !== $id && self::TRANSIENT_DB_UPDATED_LABEL !== $this->getTransientDB() ) {
+
+        if( add_post_meta( $id, 'our_ao-userid', $userId, true ) ) {
+
+          $this->logger->logInfo( 'User ID Inserted for user: ' . $userId . ' - ' . $id . ' - ' . $email . ' - ' . $name);
+          
+        } else {
+          
+          update_post_meta ( $id, 'our_ao-userid', $userId );
+
+          $this->logger->logInfo( 'User ID updated for user: ' . $userId . ' - ' . $id . ' - ' . $email . ' - ' . $name);
+        }
+
+        $this->setTransientDB( self::TRANSIENT_DB_UPDATED_LABEL );
+        
+      }
+    }
+  }
+
+  private function getTransientCSV(){
     return get_transient( self::TRANSIENT_CSV_ROWS );
   }
 
-  private function setTransient( $payload ){
-    set_transient( self::TRANSIENT_CSV_ROWS, $payload, self::TRANSIENT_EXPIRATION );
+  private function setTransientCSV( $payload ){
+    set_transient( self::TRANSIENT_CSV_ROWS, $payload, self::TRANSIENT_EXPIRATION_CSV );
+  }
+
+  private function getTransientDB(){
+    return get_transient( self::TRANSIENT_DB_UPDATED );
+  }
+
+  private function setTransientDB( $payload ){
+    set_transient( self::TRANSIENT_DB_UPDATED, $payload, self::TRANSIENT_EXPIRATION_DB_UPDATED );
   }
 
 
